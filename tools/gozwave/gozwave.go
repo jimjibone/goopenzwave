39d29fbd2ca6df11ce11dfc8b25ea0fb1c0ba85c
jimjibone <jamesreuss@gmail.com>
2015-08-27 19:57:36 +0100

    more

--- tools/gozwave/gozwave.go
+++ tools/gozwave/gozwave.go
@@ -15,51 +15,60 @@ type NodeInfo struct {
 	Values []*gozwave.ValueId
 }
 
-type NodeInfoCollector struct {
-	nodeInfo chan NodeInfo
-}
-
-func watcherFunc(notification *gozwave.Notification, userdata interface{}) {
-	notistring := notification.GetAsString()
-	fmt.Println("gozwave watcher called with notification:", notistring)
-
-	nodeInfo := NodeInfo{
-		HomeId: notification.GetHomeId(),
-		NodeId: notification.GetNodeId(),
-		// Polled: false,
-	}
-	nodeInfo.Values = append(nodeInfo.Values, notification.GetValueId())
-
-	fmt.Println("gozwave watcher NodeInfo:", nodeInfo)
-
-	switch nodeInfoCltr := userdata.(type) {
-	case NodeInfoCollector:
-		nodeInfoCltr.nodeInfo <- nodeInfo
-	default:
-		panic("userdata is an unexpected type")
-	}
-}
-
-func collectNodeInfo(nodeInfoCollector *NodeInfoCollector, nodes *map[uint8]*NodeInfo) {
-	for {
-		nodeInfo, ok := <-nodeInfoCollector.nodeInfo
-		if !ok {
-			return
-		}
-		fmt.Println("collectNodeInfo NodeInfo:", nodeInfo)
-		if node, found := (*nodes)[nodeInfo.NodeId]; found {
-			node.Values = append(node.Values, nodeInfo.Values...)
-		} else {
-			(*nodes)[nodeInfo.NodeId] = &nodeInfo
-		}
-	}
-}
+//type NodeInfoCollector struct {
+//	nodeInfo chan NodeInfo
+//}
+//
+//func watcherFunc(notification *gozwave.Notification, userdata interface{}) {
+//	notistring := notification.GetAsString()
+//	fmt.Println("gozwave watcher called with notification:", notistring)
+//
+//	nodeInfo := NodeInfo{
+//		HomeId: notification.GetHomeId(),
+//		NodeId: notification.GetNodeId(),
+//		// Polled: false,
+//	}
+//	nodeInfo.Values = append(nodeInfo.Values, notification.GetValueId())
+//
+//	fmt.Println("gozwave watcher NodeInfo:", nodeInfo)
+//
+//	switch nodeInfoCltr := userdata.(type) {
+//	case NodeInfoCollector:
+//		nodeInfoCltr.nodeInfo <- nodeInfo
+//	default:
+//		panic("userdata is an unexpected type")
+//	}
+//}
+//
+//func collectNodeInfo(nodeInfoCollector *NodeInfoCollector, nodes *map[uint8]*NodeInfo) {
+//	for {
+//		nodeInfo, ok := <-nodeInfoCollector.nodeInfo
+//		if !ok {
+//			return
+//		}
+//		fmt.Println("collectNodeInfo NodeInfo:", nodeInfo)
+//		if node, found := (*nodes)[nodeInfo.NodeId]; found {
+//			node.Values = append(node.Values, nodeInfo.Values...)
+//		} else {
+//			(*nodes)[nodeInfo.NodeId] = &nodeInfo
+//		}
+//	}
+//}
 
 func processNotifications(m *gozwave.Manager) {
 	for {
 		select {
 		case notification := <-m.Notifications:
-			fmt.Println("procprocessNotifications: ", notification.GetAsString())
+			asString := notification.GetAsString()
+			fmt.Printf("processNotifications: %q\n", asString)
+
+			nodeInfo := NodeInfo{
+				HomeId: notification.GetHomeId(),
+				NodeId: notification.GetNodeId(),
+			}
+			nodeInfo.Values = append(nodeInfo.Values, notification.GetValueId())
+
+			fmt.Println("\tNodeInfo:", nodeInfo)
 		}
 	}
 }
@@ -108,7 +117,7 @@ func main() {
 	<-sig
 
 	// All done now finish up.
-	manager.RemoveDriver("/dev/tty.usbmodem411")
+	manager.RemoveDriver(controllerPath)
 	// manager.RemoveWatcher(watcher)
 	manager.StopNotifications()
 	gozwave.DestroyManager()
