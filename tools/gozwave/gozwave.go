8ab3e8330bf438b87af680c7fd8149a5bb5a87da
jimjibone <jamesreuss@gmail.com>
2015-08-31 00:23:28 +0100

    collect and print information about nodes

--- tools/gozwave/gozwave.go
+++ tools/gozwave/gozwave.go
@@ -8,13 +8,49 @@ import (
 	"os/signal"
 )
 
+type NodeInfo struct {
+	HomeID uint32
+	NodeID uint8
+	Values []*gozwave.ValueID
+}
+
+var Nodes map[uint8]*NodeInfo
+
 func processNotifications(m *gozwave.Manager) {
 	for {
 		select {
 		case notification := <-m.Notifications:
 			fmt.Println(notification)
+
+			// Switch based on notification type.
+			switch notification.Type {
+			case gozwave.NotificationTypeNodeAdded:
+				// Create a NodeInfo from the notification then add it to the
+				// map.
+				nodeinfo := &NodeInfo{
+					HomeID: notification.HomeID,
+					NodeID: notification.NodeID,
+				}
+				Nodes[nodeinfo.NodeID] = nodeinfo
+
+			case gozwave.NotificationTypeNodeRemoved:
+				// Remove the NodeInfo from the map.
+				if _, found := Nodes[notification.NodeID]; found {
+					delete(Nodes, notification.NodeID)
+				}
+
+			case gozwave.NotificationTypeValueAdded:
+				// Find the NodeInfo in the map and add the ValueID to it.
+				if node, found := Nodes[notification.NodeID]; found {
+					node.Values = append(node.Values, notification.ValueID)
+				}
+			}
 		}
 	}
+}
+
+func init() {
+	Nodes = make(map[uint8]*NodeInfo)
 }
 
 func main() {
@@ -55,6 +91,12 @@ func main() {
 	manager.StopNotifications()
 	gozwave.DestroyManager()
 	gozwave.DestroyOptions()
+
+	// Print out what we know about the network.
+	fmt.Println("Nodes:")
+	for id, node := range Nodes {
+		fmt.Printf("\t%d: %+v\n", id, *node)
+	}
 
 	fmt.Println("finished")
 }
