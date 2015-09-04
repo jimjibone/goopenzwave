9fb89040e481716a6c8e1281da164038e4b54ab1
jimjibone <jamesreuss@gmail.com>
2015-09-04 17:35:53 +0100

    pulled out RGB light code

--- tools/gozwave/gozwave.go
+++ tools/gozwave/gozwave.go
@@ -6,7 +6,6 @@ import (
 	"gitlab.com/jimjibone/gozwave"
 	"os"
 	"os/signal"
-	"time"
 )
 
 type NodeInfo struct {
@@ -14,7 +13,6 @@ type NodeInfo struct {
 	NodeID uint8
 	Node   *gozwave.Node
 	Values map[uint64]*gozwave.ValueID
-	//TODO use (and create) Value type with embedded ValueID.
 }
 
 var Nodes = make(map[uint8]*NodeInfo)
@@ -62,26 +60,20 @@ func processNotifications(m *gozwave.Manager) {
 
 			case gozwave.NotificationTypeAwakeNodesQueried, gozwave.NotificationTypeAllNodesQueried, gozwave.NotificationTypeAllNodesQueriedSomeDead:
 				// The initial node query has completed.
-				fmt.Println("about to send InitialQueryComplete flag...")
 				if sentInitialQueryComplete == false {
 					InitialQueryComplete <- true
 				}
-				fmt.Println("...done")
 			}
 		}
 	}
 }
-
-//func init() {
-//	Nodes = make(map[uint8]*NodeInfo)
-//}
 
 func main() {
 	var controllerPath string
 	flag.StringVar(&controllerPath, "controller", "/dev/ttyACM0", "the path to your controller device")
 	flag.Parse()
 
-	fmt.Println("gozwave example starting with openzwave version:", gozwave.GetManagerVersionAsString())
+	fmt.Println("gominozw example starting with openzwave version:", gozwave.GetManagerVersionAsString())
 
 	// Setup the OpenZWave library.
 	options := gozwave.CreateOptions("/usr/local/etc/openzwave/", "", "")
@@ -131,61 +123,6 @@ func main() {
 	fmt.Println("Initial scan complete. Now polling for updates...")
 	fmt.Println("Hit ctrl-c to quit")
 
-	if lightNode, found := Nodes[2]; found {
-		time.Sleep(5 * time.Second)
-		fmt.Println("swithcing light")
-		for _, valueid := range lightNode.Values {
-			if manager.GetValueLabel(valueid) == "Level" {
-				manager.SetValueUint8(valueid, 255)
-				fmt.Println("set value Level 255")
-			}
-		}
-	}
-
-	if lightNode, found := Nodes[2]; found {
-		time.Sleep(5 * time.Second)
-		fmt.Println("swithcing light colour")
-		for _, valueid := range lightNode.Values {
-			if manager.GetValueLabel(valueid) == "Color" {
-				manager.SetValueString(valueid, "#00FF000000")
-				fmt.Println("set value Color Lime")
-			}
-		}
-	}
-
-	if lightNode, found := Nodes[2]; found {
-		time.Sleep(5 * time.Second)
-		fmt.Println("swithcing light colour")
-		for _, valueid := range lightNode.Values {
-			if manager.GetValueLabel(valueid) == "Color" {
-				manager.SetValueString(valueid, "#00000000FF")
-				fmt.Println("set value Color CW")
-			}
-		}
-	}
-
-	if lightNode, found := Nodes[2]; found {
-		time.Sleep(5 * time.Second)
-		fmt.Println("swithcing light colour")
-		for _, valueid := range lightNode.Values {
-			if manager.GetValueLabel(valueid) == "Color" {
-				manager.SetValueString(valueid, "#000000FF00")
-				fmt.Println("set value Color WW")
-			}
-		}
-	}
-
-	if lightNode, found := Nodes[2]; found {
-		time.Sleep(5 * time.Second)
-		fmt.Println("swithcing light")
-		for _, valueid := range lightNode.Values {
-			if manager.GetValueLabel(valueid) == "Level" {
-				manager.SetValueUint8(valueid, 0)
-				fmt.Println("set value Level 0")
-			}
-		}
-	}
-
 	// Now wait for the user to quit.
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
@@ -205,6 +142,4 @@ func main() {
 	manager.StopNotifications()
 	gozwave.DestroyManager()
 	gozwave.DestroyOptions()
-
-	fmt.Println("finished")
 }
