b1676f3bfc18cec8157771ec81a1fe4ad92d09ec
jimjibone <jamesreuss@gmail.com>
2015-09-06 22:00:46 +0100

    added getters/setters to ValueID

--- valueid.go
+++ valueid.go
@@ -92,6 +92,8 @@ type ValueID struct {
 	ID             uint64
 }
 
+type ValueIDStringID string
+
 func buildValueID(v C.valueid_t) *ValueID {
 	valueid := &ValueID{
 		HomeID:         uint32(C.valueid_getHomeId(v)),
@@ -104,6 +106,10 @@ func buildValueID(v C.valueid_t) *ValueID {
 		ID:             uint64(C.valueid_getId(v)),
 	}
 	return valueid
+}
+
+func (v *ValueID) StringID() ValueIDStringID {
+	return ValueIDStringID(fmt.Sprintf("%d", v.ID))
 }
 
 func (v *ValueID) toC() C.valueid_t {
@@ -128,4 +134,184 @@ func (v *ValueID) InfoString() string {
 		manager.GetValueLabel(v), manager.GetValueUnits(v),
 		manager.GetValueHelp(v), manager.GetValueMin(v), manager.GetValueMax(v),
 		valueString)
+}
+
+func (v *ValueID) GetValueLabel(valueid *ValueID) string {
+	manager := GetManager()
+	return manager.GetValueLabel(valueid)
+}
+
+func (v *ValueID) SetValueLabel(valueid *ValueID, label string) {
+	manager := GetManager()
+	manager.SetValueLabel(valueid, label)
+}
+
+func (v *ValueID) GetValueUnits(valueid *ValueID) string {
+	manager := GetManager()
+	return manager.GetValueUnits(valueid)
+}
+
+func (v *ValueID) SetValueUnits(valueid *ValueID, units string) {
+	manager := GetManager()
+	manager.SetValueUnits(valueid, units)
+}
+
+func (v *ValueID) GetValueHelp(valueid *ValueID) string {
+	manager := GetManager()
+	return manager.GetValueHelp(valueid)
+}
+
+func (v *ValueID) SetValueHelp(valueid *ValueID, help string) {
+	manager := GetManager()
+	manager.SetValueHelp(valueid, help)
+}
+
+func (v *ValueID) GetValueMin(valueid *ValueID) int32 {
+	manager := GetManager()
+	return manager.GetValueMin(valueid)
+}
+
+func (v *ValueID) GetValueMax(valueid *ValueID) int32 {
+	manager := GetManager()
+	return manager.GetValueMax(valueid)
+}
+
+func (v *ValueID) IsValueReadOnly(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.IsValueReadOnly(valueid)
+}
+
+func (v *ValueID) IsValueWriteOnly(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.IsValueWriteOnly(valueid)
+}
+
+func (v *ValueID) IsValueSet(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.IsValueSet(valueid)
+}
+
+func (v *ValueID) IsValuePolled(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.IsValuePolled(valueid)
+}
+
+func (v *ValueID) GetValueAsBool(valueid *ValueID) (bool, bool) {
+	manager := GetManager()
+	return manager.GetValueAsBool(valueid)
+}
+
+func (v *ValueID) GetValueAsByte(valueid *ValueID) (bool, byte) {
+	manager := GetManager()
+	return manager.GetValueAsByte(valueid)
+}
+
+func (v *ValueID) GetValueAsFloat(valueid *ValueID) (bool, float32) {
+	manager := GetManager()
+	return manager.GetValueAsFloat(valueid)
+}
+
+func (v *ValueID) GetValueAsInt(valueid *ValueID) (bool, int32) {
+	manager := GetManager()
+	return manager.GetValueAsInt(valueid)
+}
+
+func (v *ValueID) GetValueAsShort(valueid *ValueID) (bool, int16) {
+	manager := GetManager()
+	return manager.GetValueAsShort(valueid)
+}
+
+func (v *ValueID) GetValueAsString(valueid *ValueID) (bool, string) {
+	manager := GetManager()
+	return manager.GetValueAsString(valueid)
+}
+
+func (v *ValueID) GetValueAsRaw(valueid *ValueID) (bool, []byte) {
+	manager := GetManager()
+	return manager.GetValueAsRaw(valueid)
+}
+
+func (v *ValueID) GetValueListSelectionAsString(valueid *ValueID) (bool, string) {
+	manager := GetManager()
+	return manager.GetValueListSelectionAsString(valueid)
+}
+
+func (v *ValueID) GetValueListSelectionAsInt32(valueid *ValueID) (bool, int32) {
+	manager := GetManager()
+	return manager.GetValueListSelectionAsInt32(valueid)
+}
+
+func (v *ValueID) GetValueListItems(valueid *ValueID) (bool, []string) {
+	manager := GetManager()
+	return manager.GetValueListItems(valueid)
+}
+
+func (v *ValueID) GetValueFloatPrecision(valueid *ValueID) (bool, uint8) {
+	manager := GetManager()
+	return manager.GetValueFloatPrecision(valueid)
+}
+
+func (v *ValueID) SetValueBool(valueid *ValueID, value bool) bool {
+	manager := GetManager()
+	return manager.SetValueBool(valueid, value)
+}
+
+func (v *ValueID) SetValueUint8(valueid *ValueID, value uint8) bool {
+	manager := GetManager()
+	return manager.SetValueUint8(valueid, value)
+}
+
+func (v *ValueID) SetValueFloat(valueid *ValueID, value float32) bool {
+	manager := GetManager()
+	return manager.SetValueFloat(valueid, value)
+}
+
+func (v *ValueID) SetValueInt32(valueid *ValueID, value int32) bool {
+	manager := GetManager()
+	return manager.SetValueInt32(valueid, value)
+}
+
+func (v *ValueID) SetValueInt16(valueid *ValueID, value int16) bool {
+	manager := GetManager()
+	return manager.SetValueInt16(valueid, value)
+}
+
+func (v *ValueID) SetValueBytes(valueid *ValueID, value []byte) bool {
+	manager := GetManager()
+	return manager.SetValueBytes(valueid, value)
+}
+
+func (v *ValueID) SetValueString(valueid *ValueID, value string) bool {
+	manager := GetManager()
+	return manager.SetValueString(valueid, value)
+}
+
+func (v *ValueID) SetValueListSelection(valueid *ValueID, selectedItem string) bool {
+	manager := GetManager()
+	return manager.SetValueListSelection(valueid, selectedItem)
+}
+
+func (v *ValueID) RefreshValue(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.RefreshValue(valueid)
+}
+
+func (v *ValueID) SetChangeVerified(valueid *ValueID, verify bool) {
+	manager := GetManager()
+	manager.SetChangeVerified(valueid, verify)
+}
+
+func (v *ValueID) GetChangeVerified(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.GetChangeVerified(valueid)
+}
+
+func (v *ValueID) PressButton(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.PressButton(valueid)
+}
+
+func (v *ValueID) ReleaseButton(valueid *ValueID) bool {
+	manager := GetManager()
+	return manager.ReleaseButton(valueid)
 }
